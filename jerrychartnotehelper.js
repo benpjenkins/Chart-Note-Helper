726b0d19e73e72499aae6d38e45456fbb2721902
benpjenkins <46969132+benpjenkins@users.noreply.github.com>
2019-01-28 20:02:40 -0600

    Update jerrychartnotehelper.js
    Previous version didn't correctly access the user input from html.  This version establishes the basic structure to concatenate the string but does not actually receive the input from html yet.

--- jerrychartnotehelper.js
+++ jerrychartnotehelper.js
@@ -1,15 +1,22 @@
-let chartnote = "Screening visit by Spiritual Care Volunteer Jerry Black  ";
+let chartnote = "Screening visit by Spiritual Care Volunteer Jerry Black\n";
 
-function addToChart() {
+function addToChart(position, disposition, unique, support, need, card, chaplain) {
   //helper function to add user input to user selected input
-  if (document.getElementById("charthelper").position.value !== null) {  
-    var position = document.getElementById("charthelper").position.value;
-    chartnote += "Pt was " + position + " at time of chaplain visit.";
-  }
+  /*next goal is to add correct ids to html and access that information in js
+    This may mean reworking the helper function or breaking it up into several.
+    Current parameters will probably need to be stored as variables instead.
+    Consider using an object to store user inputs as well.
+  */
+  chartnote += "Pt was ${position} and ${disposition} at time of visit.  ";
+  chartnote += "${unique}.  ";
+  chartnote += "Pt relies on their ${support} for support.  "
+  chartnote += "Pt desires to ${need}.  ";
+  chartnote += "Volunter left chaplain card.\n";
+  chartnote += "Entered by Resident Chaplain ${chaplain}";
 }
 
 function submit() {
   addToChart();
   alert(chartnote);
+  //this function needs the ability to copy chartnote to the clipboard
 }
-
