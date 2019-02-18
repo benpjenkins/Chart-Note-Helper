920f95fea0d1ede957365eacccfcc89f89e2960f
benpjenkins <46969132+benpjenkins@users.noreply.github.com>
2019-02-18 16:04:55 -0600

    Update jerrychartnotehelper.js

--- jerrychartnotehelper.js
+++ jerrychartnotehelper.js
@@ -1,22 +1,23 @@
-let chartnote = "Screening visit by Spiritual Care Volunteer Jerry Black\n";
-
-function addToChart(position, disposition, unique, support, need, card, chaplain) {
-  //helper function to add user input to user selected input
-  /*next goal is to add correct ids to html and access that information in js
-    This may mean reworking the helper function or breaking it up into several.
-    Current parameters will probably need to be stored as variables instead.
-    Consider using an object to store user inputs as well.
-  */
-  chartnote += "Pt was ${position} and ${disposition} at time of visit.  ";
-  chartnote += "${unique}.  ";
-  chartnote += "Pt relies on their ${support} for support.  "
-  chartnote += "Pt desires to ${need}.  ";
-  chartnote += "Volunter left chaplain card.\n";
-  chartnote += "Entered by Resident Chaplain ${chaplain}";
-}
 
 function submit() {
-  addToChart();
+  let chartnote = "Screening visit by Spiritual Care Volunteer Jerry Black\n\n";
+
+  let unique = document.getElementById("unique_text").value;
+  let position = document.getElementById("position_text").value
+  let disposition = document.getElementById("disposition_text").value
+  let support = document.getElementById("support_text").value;
+  let need = document.getElementById("need_text").value
+  let chaplain = document.getElementById("chaplain_text").value
+  let action = document.getElementById("action_text").value
+  
+  
+  chartnote += `Pt was ${position} and ${disposition} at time of visit.  `;
+  chartnote += `Pt was unique because ${unique}.  `
+  chartnote += `Pt relies on their ${support} for support.  `
+  chartnote += `Pt desires to ${need}.  `;
+  chartnote += `Volunter ${action}.\n\n`;
+  chartnote += `Entered by Resident Chaplain ${chaplain}`;
   alert(chartnote);
-  //this function needs the ability to copy chartnote to the clipboard
+
 }
+
